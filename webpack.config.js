38ae329a5421731ee4a6544fee718fc153842718
Douglas Machado <douglas.machado@valor-software.com>
2023-02-15 11:34:38 -0300

    Added Nativescript testIDs

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,12 @@ const webpack = require("@nativescript/webpack");
 
 module.exports = (env) => {
 	webpack.init(env);
+	
+	let isProduction = !!env.production;
+	if (!isProduction) {
+		// enable testID to e2e test (Appium)
+		env.e2e = true;
+	}
 
 	return webpack.resolveConfig();
 };
-
-
